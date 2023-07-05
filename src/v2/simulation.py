c56c00fb902e0ebdbe3ef48483e987c596fa781a
João Pedro Holanda <jpholanda.prf@gmail.com>
2023-07-04 21:52:18 -0300

    Use stages in Simulation

--- src/v2/simulation.py
+++ src/v2/simulation.py
@@ -1,6 +1,8 @@
+import random
+
 from .pile import TaskPile
+from .stage import Stage
 from .task import Task
-from .worker import Worker
 
 
 class Simulation:
@@ -11,17 +13,25 @@ class Simulation:
             TaskPile()
         ]
 
-        self.workers = [
-            Worker(0, precision=0.5, input_pile=self.piles[0], output_pile=self.piles[1]),
-            Worker(1, precision=0.5, input_pile=self.piles[1], output_pile=self.piles[2])
+        self.stages = [
+            Stage(stage_id=0,
+                  num_workers=1,
+                  precision_distribution=lambda: 0.5,
+                  input_pile=self.piles[0],
+                  output_pile=self.piles[1]),
+            Stage(stage_id=1,
+                  num_workers=1,
+                  precision_distribution=lambda: 0.5,
+                  input_pile=self.piles[1],
+                  output_pile=self.piles[2]),
         ]
 
         self.piles[0].put(*[Task() for _ in range(10)])
 
     def run(self):
         for i in range(8):
-            for worker in self.workers:
-                worker.step()
+            for stage in self.stages:
+                stage.step()
 
         for task in self.piles[2]:
             print(task)
