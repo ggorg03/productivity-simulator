677150129fa4499ea965bb1ce139556b6f4366d6
Gustavo Gorgônio <ggorgon03@gmail.com>
2023-07-07 22:59:09 -0300

    add paramns to have more data for our study

--- src/v2/task.py
+++ src/v2/task.py
@@ -2,15 +2,22 @@ class Task:
     FAILED = "Failed"
     COMPLETED = "Completed"
 
-    def __init__(self, num_stages: int = 2):
+    def __init__(self, task_id: int = None, num_stages: int = 2):
+        self.id = task_id
+        self.stage_id = None
+        self.in_pipeline = True
         self.tries = 0
         self.outcomes = [None for _ in range(num_stages)]
 
     def set_outcome(self, stage_id, state):
         self.outcomes[stage_id] = state
+        self.stage_id = stage_id
 
     def get_outcome(self, stage_id):
         return self.outcomes[stage_id]
+    
+    def set_out_pipeline(self):
+        self.in_pipeline = False
 
     def fully_complete(self) -> bool:
         for outcome in self.outcomes:
